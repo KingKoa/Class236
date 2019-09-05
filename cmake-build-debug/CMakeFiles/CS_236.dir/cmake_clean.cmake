9a28f4885fafe2df51afbbe663ae740728fe5eb9
Kekoapo'aono Montalbo <kekoapoaonomontalbo@KekoapoaonosAir.app.byu.edu>
2019-09-05 16:03:15 -0600

    Here we go with the Git Hub Thing again

---
+++ cmake-build-debug/CMakeFiles/CS_236.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_236.dir/main.cpp.o"
+  "CS_236.pdb"
+  "CS_236"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS_236.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
